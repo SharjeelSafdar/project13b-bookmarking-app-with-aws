49b7e603859f1500e6aeb1c540850b698427df9d
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-18 17:40:22 +0500

    Updated mapping tamplates for appsync resolvers

--- backend/lib/backend-services-stack.ts
+++ backend/lib/backend-services-stack.ts
@@ -6,7 +6,7 @@ export class ServicesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bookmarksTable = new ddb.Table(this, "BookmarksTable", {
+    const bookmarksTable = new ddb.Table(this, "P13bBookmarksTable", {
       partitionKey: {
         name: "id",
         type: ddb.AttributeType.STRING,
@@ -15,8 +15,8 @@ export class ServicesStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const gqlApi = new appsync.GraphqlApi(this, "GraphQlApi", {
-      name: "GraphQL-Api",
+    const gqlApi = new appsync.GraphqlApi(this, "P13bGraphQlApi", {
+      name: "P13b-GraphQL-Api",
       schema: appsync.Schema.fromAsset("graphql/schema.gql"),
       authorizationConfig: {
         defaultAuthorization: {
@@ -29,7 +29,7 @@ export class ServicesStack extends cdk.Stack {
     });
 
     const ddbDataSource = gqlApi.addDynamoDbDataSource(
-      "DdbDataSource",
+      "P13bDynamoDbDataSource",
       bookmarksTable
     );
     bookmarksTable.grantReadWriteData(ddbDataSource);
@@ -41,72 +41,51 @@ export class ServicesStack extends cdk.Stack {
       responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultList(),
     });
 
-    // ddbDataSource.createResolver({
-    //   typeName: "Mutation",
-    //   fieldName: "createBookmark",
-    //   requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
-    //     appsync.PrimaryKey.partition("id").auto(),
-    //     appsync.Values.projecting("$ctx.args")
-    //   ),
-    //   // requestMappingTemplate: appsync.MappingTemplate.fromString(`
-    //   //   {
-    //   //     "version": "2017-02-28",
-    //   //     "operation": "PutItem",
-    //   //     "key": {
-    //   //       "id": $util.dynamodb.toDynamoDBJson($util.autoId())
-    //   //     },
-    //   //     "attributeValues": $util.dynamodb.toDynamoDBJson($ctx.args)
-    //   //   }
-    //   // `),
-    //   responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
-    // });
+    ddbDataSource.createResolver({
+      typeName: "Mutation",
+      fieldName: "createBookmark",
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
+        appsync.PrimaryKey.partition("id").auto(),
+        appsync.Values.projecting()
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
 
-    // ddbDataSource.createResolver({
-    //   typeName: "Mutation",
-    //   fieldName: "editBookmark",
-    //   requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
-    //     appsync.PrimaryKey.partition("id").is(
-    //       appsync.AttributeValues.arguments("id")
-    //     ),
-    //     appsync.Values.projecting("$ctx.args")
-    //   ),
-    //   // requestMappingTemplate: appsync.MappingTemplate.fromString(`
-    //   //   {
-    //   //     "version": "2017-02-28",
-    //   //     "operation": "UpdateItem",
-    //   //     "key": {
-    //   //       "id": $util.dynamodb.toDynamoDBJson($ctx.args.id)
-    //   //     },
-    //   //     "update": {
-    //   //       "expression": "SET title = :newTitle, url = :newUrl",
-    //   //       "expressionValues": {
-    //   //         ":newContent": $util.dynamodb.toDynamoDBJson($ctx.args.title),
-    //   //         ":newUrl": $util.dynamodb.toDynamoDBJson($ctx.args.url)
-    //   //       }
-    //   //     }
-    //   //   }
-    //   // `),
-    //   responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
-    // });
+    ddbDataSource.createResolver({
+      typeName: "Mutation",
+      fieldName: "editBookmark",
+      requestMappingTemplate: appsync.MappingTemplate.fromString(`
+        {
+          "version": "2017-02-28",
+          "operation": "UpdateItem",
+          "key": {
+            "id": $util.dynamodb.toDynamoDBJson($ctx.args.id)
+          },
+          "update": {
+            "expression": "SET #oldTitle = :newTitle, #oldUrl = :newUrl",
+            "expressionNames": {
+              "#oldTitle": "title",
+              "#oldUrl": "url"
+            },
+            "expressionValues": {
+              ":newTitle": $util.dynamodb.toDynamoDBJson($ctx.args.title),
+              ":newUrl": $util.dynamodb.toDynamoDBJson($ctx.args.url)
+            }
+          }
+        }
+      `),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
 
-    // ddbDataSource.createResolver({
-    //   typeName: "Mutation",
-    //   fieldName: "deleteBookmark",
-    //   requestMappingTemplate: appsync.MappingTemplate.dynamoDbDeleteItem(
-    //     "id",
-    //     appsync.Values.arguments("id")
-    //   ),
-    //   // requestMappingTemplate: appsync.MappingTemplate.fromString(`
-    //   //   {
-    //   //     "version": "2017-02-28",
-    //   //     "operation": "DeleteItem",
-    //   //     "key": {
-    //   //       "id": $util.dynamodb.toDynamoDBJson($ctx.args.id)
-    //   //     }
-    //   //   }
-    //   // `),
-    //   responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
-    // });
+    ddbDataSource.createResolver({
+      typeName: "Mutation",
+      fieldName: "deleteBookmark",
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbDeleteItem(
+        "id",
+        "id"
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
 
     new cdk.CfnOutput(this, "P13bGraphQLApiId", {
       value: gqlApi.apiId,
